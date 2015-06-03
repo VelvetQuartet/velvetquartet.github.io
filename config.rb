997194c70312dbe24c3c09a44fcba13cd17cea2d
Narain Darakananda <naraindarakananda@gmail.com>
2015-06-02 22:34:15 -0700

    Move build directory outside

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'breakpoint'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "build/css"
+css_dir = "./css"
 sass_dir = "src/sass"
 images_dir = "images"
-javascripts_dir = "build/js"
+javascripts_dir = "./js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
